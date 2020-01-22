1517ae539c94eee9111f3fde4fd7afd9383ae087
mdanf4sn <danfakhamh@gmail.com>
2020-01-22 13:59:50 +0000

    Chargemeny fichier du projet

---
+++ src/main/java/com/stma/test_Stater/TestStaterApplication.java
@@ -0,0 +1,13 @@
+package com.stma.test_Stater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stma/test_Stater/TestStaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stma.test_Stater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
